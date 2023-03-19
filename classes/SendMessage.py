c54c9ff389d9dce36404fbf0f952b70eb26c52e6
Abdelrahman Samir <abdelrahman@notchnco.com>
2023-03-19 13:04:13 +0200

    Added Telegram Send Message

---
+++ classes/SendMessage.py
@@ -0,0 +1,22 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+class SendMessage:
+    def __init__(self, chatID, message):
+        self.chatID = chatID
+        self.message = message
+
+    def send(self):
+        load_dotenv();
+        telegramToken = os.getenv("TELEGRAM_BOT_TOKEN")
+        url = f"https://api.telegram.org/bot{telegramToken}/sendMessage"
+        data = {
+            "chat_id": self.chatID,
+            "text": self.message
+        }
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            print("User created successfully!")
+        else:
+            print("Error creating user:", response.text)
---
+++ index.py
@@ -0,0 +1,4 @@
+from classes.SendMessage import SendMessage
+
+# Send Alert Message via telegram
+SendMessage("822482686", "HI").send()
