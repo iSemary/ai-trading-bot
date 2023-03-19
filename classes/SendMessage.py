a2b79fd1eb1c5615755abd831701a81b72c3a7fd
Abdelrahman Samir <abdelrahman@notchnco.com>
2023-03-19 13:38:05 +0200

    Added Telegram Send Message

--- classes/SendMessage.py
+++ classes/SendMessage.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
 
 
 class SendMessage:
@@ -19,5 +20,42 @@ class SendMessage:
         response = requests.post(url, data=data)
         if response.status_code == 200:
             print("Message Sent successfully!")
+        else:
+            print("Error sending message:", response.text)
+
+    def sendQuestionButtons(self):
+        load_dotenv()
+        telegramToken = os.getenv("TELEGRAM_BOT_TOKEN")
+        url = f"https://api.telegram.org/bot{telegramToken}/sendMessage"
+        headers = {"Content-Type": "application/json"}
+        data = {
+            "chat_id": self.chatID,
+            "text": self.message,
+            "reply_markup": {
+                "inline_keyboard": [
+                    [
+                        {
+                            "text": "Button 1",
+                            "callback_data": "button_1"
+                        },
+                        {
+                            "text": "Button 2",
+                            "callback_data": "button_2"
+                        }
+                    ],
+                    [
+                        {
+                            "text": "Button 3",
+                            "callback_data": "button_3"
+                        }
+                    ]
+                ]
+            }
+        }
+
+        data = json.dumps(data)
+        response = requests.post(url, headers=headers, data=data)
+        if response.status_code == 200:
+            print("Button Message Sent successfully!")
         else:
             print("Error sending message:", response.text)
--- index.py
+++ index.py
@@ -2,3 +2,4 @@ from classes.SendMessage import SendMessage
 
 # Send Alert Message via telegram
 SendMessage("822482686", "HI").send()
+SendMessage("822482686", "HI").sendQuestionButtons()
