18b3c93b264ebe45c17ace07e683b6f6fa808817
Abdelrahman Samir <abdelrahman@notchnco.com>
2023-03-22 14:10:04 +0200

    added rsi strategy sample

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from classes.SendMessage import SendMessage
 
-# Send Alert Message via telegram
-SendMessage("822482686", "HI").send()
-SendMessage("822482686", "HIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHI").sendQuestionButtons(['Sell', 'Buy'])
+# # Send Alert Message via telegram
+# SendMessage("822482686", "HI").send()
+# SendMessage("822482686", "HIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHI").sendQuestionButtons(['Sell', 'Buy'])
+
